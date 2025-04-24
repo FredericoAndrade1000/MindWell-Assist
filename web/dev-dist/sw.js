2afa131276a167dda79095b4e99bb3ea5b76e061
ByteMax-EI <fredericoandrade.bytemax@gmail.com>
2025-04-24 19:55:27 -0300

    Update cron job logging in app.log, modify service worker revision in sw.js, and update database.sqlite. Enhance logging for scheduled tasks and ensure service worker reflects the latest changes.

--- web/dev-dist/sw.js
+++ web/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-cc2c504f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.kem74co61jo"
+    "revision": "0.3frmuonj5n8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
