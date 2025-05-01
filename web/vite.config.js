5f593e184fae585c7e1200eb46cc3dc5a4be6747
ByteMax-EI <fredericoandrade.bytemax@gmail.com>
2025-05-01 00:23:12 -0300

    Enhance Vite configuration by adding maximum file size limit for caching and setting server host to '0.0.0.0' for improved accessibility.

--- web/vite.config.js
+++ web/vite.config.js
@@ -39,6 +39,7 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2,woff,jpg,mp3}'], // Adicionado jpg, mp3
+        maximumFileSizeToCacheInBytes: 2 * 1024 * 1024,
         runtimeCaching: [
           {
             urlPattern: ({ request }) => request.destination === 'document',
@@ -136,6 +137,7 @@ export default defineConfig({
   ],
   server: {
     port: 5173,
+    host: '0.0.0.0',
     strictPort: true,
   },
   test: {
